3fea651573577d74c548e4ef838a3bc8cfec4382
Pavlo Fedas <pavlofedas25@gmail.com>
2025-07-28 04:08:24 +0300

    4hw10

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,6 +1,9 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
 const input = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('button[data-start]');
 const daysSpan = document.querySelector('[data-days]');
